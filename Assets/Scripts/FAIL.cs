b16d87c7d95d2495601bae08f883e73b8af3b8f3
Alan Mendez <iipenguinii@yahoo.com>
2019-02-10 20:58:26 -0800

    First commit

---
+++ Assets/Scripts/FAIL.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class FAIL : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Plane"))
+        {
+            //other.gameObject.SetActive(false);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 0);
+        }
+    }
+}
